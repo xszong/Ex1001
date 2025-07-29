d26186065610cbb4b009b62a64f8ad148edae97e
Xszong <987844869@qq.com>
2025-07-29 23:21:17 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Ex1001
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// 設計工具所需的變數。
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// 清除任何使用中的資源。
+      /// </summary>
+      /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form 設計工具產生的程式碼
+
+      /// <summary>
+      /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+      /// 這個方法的內容。
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(28, 71);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(5);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(109, 35);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "顯示";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDisplay.ForeColor = System.Drawing.Color.Blue;
+            this.lblDisplay.Location = new System.Drawing.Point(40, 22);
+            this.lblDisplay.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(117, 26);
+            this.lblDisplay.TabIndex = 1;
+            this.lblDisplay.Text = "Label控制項";
+            this.lblDisplay.Click += new System.EventHandler(this.lblDisplay_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(159, 71);
+            this.btnEnd.Margin = new System.Windows.Forms.Padding(5);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(109, 35);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "結束";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 113);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnShow);
+            this.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "显示信息";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button btnShow;
+      private System.Windows.Forms.Label lblDisplay;
+      private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System; // 引入基本的类和功能
+using System.Collections.Generic; // 引入集合类的命名空间（未在本程序中使用）
+using System.ComponentModel; // 引入组件模型的命名空间（未在本程序中使用）
+using System.Data; // 引入数据相关的命名空间（未在本程序中使用）
+using System.Drawing; // 引入图形相关的命名空间（未在本程序中使用）
+using System.Linq; // 引入 LINQ 查询功能的命名空间（未在本程序中使用）
+using System.Text; // 引入字符串处理的命名空间（未在本程序中使用）
+using System.Threading.Tasks; // 引入异步编程的命名空间（未在本程序中使用）
+using System.Windows.Forms; // 引入 Windows Forms 应用程序所需的命名空间
+
+namespace Ex1001 // 定义一个命名空间 Ex1001
+{
+    public partial class Form1 : Form // 定义一个名为 Form1 的类，继承自 Form
+    {
+        public Form1() // 构造函数
+        {
+            InitializeComponent(); // 初始化组件，设置窗体及其控件
+        }
+
+        // 按钮点击事件处理程序
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            // 获取当前的日期和时间，并将其转换为字符串
+            lblDisplay.Text = DateTime.Now.ToString(); // 在标签上显示当前日期和时间
+        }
+
+        // 结束应用程序的按钮事件处理程序
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // 关闭应用程序
+        }
+
+        // 标签点击事件处理程序（当前未使用）
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 窗体加载事件处理程序（当前未使用）
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//构造函数：
+
+//Form1()：调用 InitializeComponent() 方法来初始化窗体和控件。
+//按钮事件：
+
+//btnShow_Click：当用户点击 "Show" 按钮时，获取当前日期和时间并显示在标签 lblDisplay 上。
+//btnEnd_Click：当用户点击 "End" 按钮时，调用 Application.Exit() 结束应用程序。
+//未使用的事件：
+
+//lblDisplay_Click 和 Form1_Load 方法当前未实现任何功能，但它们可以用于处理标签点击事件和窗体加载事件。
+//注意事项
+//控件初始化：InitializeComponent() 方法是自动生成的，用于初始化窗体上的控件。
+//事件处理：确保将事件处理程序正确地连接到窗体上的控件，例如按钮和标签。
